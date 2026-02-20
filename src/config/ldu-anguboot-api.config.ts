c41fa18295f6d23578ab42c2deaa77e311479259
lild <lildworks@gmail.com>
2026-02-20 15:00:15 +0100

    FEAT: LDu AnguBoot API Url config

--- src/config/ldu-anguboot-api.config.ts
+++ src/config/ldu-anguboot-api.config.ts
@@ -3,3 +3,7 @@ import { InjectionToken } from '@angular/core';
 export const baseApiUrl = import.meta.env?.VITE_API_URL ?? '/api';
 
 export const API = new InjectionToken<LDuAnguBootApiConfig>('API');
+
+export const API_CONFIG: LDuAnguBootApiConfig = {
+    url: baseApiUrl
+};
